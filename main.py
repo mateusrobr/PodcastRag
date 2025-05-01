fb67e6721a67a1f28aacb7145c8df50fc062f760
Mateus Pereira <mateusrobr@gmail.com>
2025-05-01 16:11:21 -0300

    subtitle parsing and grouping for chunking and embbeding

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-from audio.subtitles import get_subtitles
-teste = get_subtitles("https://www.youtube.com/watch?v=EXOxGXe09GI")
-print(teste)
+from subtitles.subtitles import get_subtitles,parse_srt_string, group_subtitle_texts
+import re
+teste = get_subtitles("https://www.youtube.com/watch?v=FAyKDaXEAgc", "a.en")
+parsed_srt = parse_srt_string(teste)
+
+all_subs = group_subtitle_texts(parsed_srt)
+
+print(all_subs)
--- subtitles/subtitles.py
+++ subtitles/subtitles.py
@@ -2,21 +2,40 @@ from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
 from pydub import AudioSegment
+import re
 
 
 
-def get_subtitles(url):
+def get_subtitles(url,caption_code):
     yt = YouTube(url,on_progress_callback=on_progress)
-    captions = yt.captions["a.pt"]
+    captions = yt.captions[caption_code]
 
     srt_subtitles = captions.generate_srt_captions()
     return srt_subtitles
 
 
-def convert_m4a_wav(filename):
-    wav_filename = 'output.wav'
-    sound = AudioSegment.from_file(filename, format='m4a')
-    file_handle = sound.export(wav_filename, format='wav')
+def parse_srt_string(srt_text):
+    blocks = re.split(r'\n{2,}', srt_text.strip())
+    parsed_text = []
+
+    for block in blocks:
+        lines = block.strip().split('\n')
+        id = lines[0]
+        timestamp = lines[1]
+        subtitle = lines[2]
+        parsed_text.append([id,timestamp,subtitle])
+
+    return parsed_text
+
+def group_subtitle_texts(srt_parsed):
+    all_subs = ""
+    for lines in srt_parsed:
+        all_subs += lines[2]
+
+    return all_subs
+
+    
+
 
 
 
